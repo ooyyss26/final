dbefa4bde51ad194e929106fd52351ae48cfb79c
zha7nea <202280050@psu.palawan.edu.ph>
2024-12-19 17:27:14 +0800

    feat: create app in api.py

---
+++ api.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity, get_jwt
+from sqlalchemy.exc import IntegrityError
+
+db = SQLAlchemy()
+jwt = JWTManager()
+
+def create_app(config=None):
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:bakanese@localhost/bank_and_branches'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['JWT_SECRET_KEY'] = 'your-secret-key'  # Change this to a more secure secret
+
+    if config:
+        app.config.update(config)
+
+    db.init_app(app)
+    jwt.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+    register_routes(app)
+    register_error_handlers(app)
+
+    return app
